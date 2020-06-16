96e3b222426510f83e014dca3e442f9adc74f10a
zhenglihui-106 <60957384+zhenglihui-106@users.noreply.github.com>
2020-06-16 22:59:44 +0800

    Add files via upload

---
+++ 选择排序.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+void selectionSort(T array[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int min_index = i;
+		for (int j = i+1; j < n; j++)
+		{
+			
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		swap(array[i], array[min_index]);
+	}
+}
+
+int main()
+{
+	
+
+	system("pause");
+	return 0;
+}
