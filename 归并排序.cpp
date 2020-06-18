074e0275f25ecb1d47698fe7bfc33c488259bf3e
zhenglihui-106 <60957384+zhenglihui-106@users.noreply.github.com>
2020-06-18 12:02:41 +0800

    重要

---
+++ 归并排序.cpp
@@ -0,0 +1,48 @@
+template<typename T>
+void __merge(T array[], int l, int r, int mid)
+{
+	T *aux = new T[r - l + 1];
+	for (int i = l; i <= r; i++)
+		aux[i - 1] = array[i];
+
+	int i = l; j = mid + 1;
+	for (int k = l; k <= r; k++)
+	{
+		if (i > mid)
+		{
+			array[k] = aux[j - l]; j++;
+		}
+		else if (j > r)
+		{
+			array[k] = aux[i - l]; i++;
+		}
+		else if (aux[i-l]<aux[j-l])
+		{
+			array[k] = aux[i - l]; i++;
+		}
+		else
+		{
+			array[k] = aux[j - l]; j++;			
+		}
+	}
+
+	delete aux;
+}
+
+template<typename T>
+void __mergeSort(T array, int l, int r)
+{
+	if (l >= r)
+		return;
+
+	int mid = (l + r) / 2;
+	__mergeSort(array, l, mid);
+	__mergeSort(array, mid + 1, r);
+	__merge(array, l, r, mid);
+}
+
+template<typename T>
+void mergeSort(T arrar[], int n)
+{
+	__mergeSort(arr, 0, n - 1);
+}
